55d5fcd6564396996b6d9a6f551805ffffd764c1
shehan <dmgcoding@gmail.com>
2024-02-03 10:44:13 +0530

    feat: ui organinzed

---
+++ lib/pages/home/views/selected_category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SelectedCategory extends StatelessWidget {
+  const SelectedCategory({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Align(
+      child: Text(
+        'All Tasks',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
